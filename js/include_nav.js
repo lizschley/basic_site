8ba850964c7e4703c87f49acafb5c800e553e476
lizschley <7227538+lizschley@users.noreply.github.com>
2026-07-17 17:39:42 -0400

    hope this works

--- js/include_nav.js
+++ js/include_nav.js
@@ -1,4 +1,4 @@
-const base_path = 'https://lizschley.github.io/basic_website/'
+const base_path = '/basic_website/'
 
 window.onload = (event) => {
     this.set_base_path()
@@ -15,7 +15,7 @@ function set_base_path() {
     if (window.location.hostpath == 'localhost') {
         let tag = document.getElementsByTagName('base')
         tag.setAttribute('href', '/')
-    } else tag.setAttribute('href', base_path)
+    }
 }
 
 
@@ -27,16 +27,16 @@ function navbar_html() {
     <div class='collapse navbar-collapse' id='navbar-collapse'>
        <ul class='navbar-liz list-unstyled me-auto mb-2 mb-sm-0'>
           <li class='nav-item'>
-             <a class='nav-link active navlink-liz' aria-current='page' href='/../index.html'>Home</a>
+             <a class='nav-link active navlink-liz' aria-current='page' href='../index.html'>Home</a>
           </li>
           <li class='nav-item dropdown'>
              <a id='nav_dropdown' class='dropdown-toggle liz-colors' href='#' data-bs-toggle='dropdown' aria-expanded='false'>Insect Habitat</a>
              <ul class='dropdown-menu'>
-                <li class='list-item'><a class='dropdown-item' href='/html/insect_habitat/plant_overview.html'>Overview of Plant Project</a></li>
-                <li class='list-item'><a class='dropdown-item ' href='/html/insect_habitat/community.html'>Home Plant Communities</a></li>
-                <li class='list-item'><a class='dropdown-item' href='/html/insect_habitat/va_native.html'>Virginia Natives</a></li>
-                <li class='list-item'><a class='dropdown-item ' href='/html/insect_habitat/native_east_us.html'>East US Natives</a></li>
-                <li class='list-item'><a class='dropdown-item' href='/html/insect_habitat/non_native.html'>Non Natives</a></li>
+                <li class='list-item'><a class='dropdown-item' href='html/insect_habitat/plant_overview.html'>Overview of Plant Project</a></li>
+                <li class='list-item'><a class='dropdown-item ' href='html/insect_habitat/community.html'>Home Plant Communities</a></li>
+                <li class='list-item'><a class='dropdown-item' href='html/insect_habitat/va_native.html'>Virginia Natives</a></li>
+                <li class='list-item'><a class='dropdown-item ' href='html/insect_habitat/native_east_us.html'>East US Natives</a></li>
+                <li class='list-item'><a class='dropdown-item' href='html/insect_habitat/non_native.html'>Non Natives</a></li>
              </ul>
           </li>
           <li class="nav-item dropdown">
@@ -47,8 +47,8 @@ function navbar_html() {
                 <li class="dropdown-submenu">
                     <a class="dropdown-item dropdown-toggle" href="#">Random</a>
                     <ul class="dropdown-menu">
-                      <li class='list-item'><a class='dropdown-item' href='/html/blog/random/2026_short.html'>Initial</a></li>
-                      <li class='list-item'><a class='dropdown-item' href='/html/blog/random/hawaii_late_sept_2025.html'>O'ahu, Hawai\'i</a></li>
+                      <li class='list-item'><a class='dropdown-item' href='html/blog/random/2026_short.html'>Initial</a></li>
+                      <li class='list-item'><a class='dropdown-item' href='html/blog/random/hawaii_late_sept_2025.html'>O'ahu, Hawai\'i</a></li>
                     </ul>
                 </li>
                 <li class="dropdown-submenu">
@@ -64,9 +64,9 @@ function navbar_html() {
           <li class='nav-item dropdown'>
              <a id='nav_dropdown' class='dropdown-toggle liz-colors' href='#' data-bs-toggle='dropdown' aria-expanded='false'>Misc Info</a>
              <ul class='dropdown-menu'>
-                <li class='list-item'><a class='dropdown-item ' href='/html/misc/philosophical_gleanings.html'>Philosophical Gleanings (compiled by WH Brenner)</a></li>
-                <li class='list-item'><a class='dropdown-item ' href='/html/misc/tech_q_a.html'>Technical from 2020</a></li>
-                <li class='list-item'><a class='dropdown-item' href='/html/misc/google_sheets_filtering.html'>Google Sheets Filtering</a></li>
+                <li class='list-item'><a class='dropdown-item ' href='html/misc/philosophical_gleanings.html'>Philosophical Gleanings (compiled by WH Brenner)</a></li>
+                <li class='list-item'><a class='dropdown-item ' href='html/misc/tech_q_a.html'>Technical from 2020</a></li>
+                <li class='list-item'><a class='dropdown-item' href='html/misc/google_sheets_filtering.html'>Google Sheets Filtering</a></li>
              </ul>
           </li>
        </ul>
